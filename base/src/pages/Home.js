509b30d67e6f1dc45cf9461307f631b63e77401b
bibek-056 <115538325+bibek-056@users.noreply.github.com>
2023-02-20 20:47:35 +0545

    Create Home.js

---
+++ base/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Components/Banner"
+import Belt from "../Components/Belt"
+
+const Home = () => {
+    return(
+        <div>
+            <Banner />
+            <Belt />
+        </div>
+    )
+}
+
+export default Home;
